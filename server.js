2e61b58c3e9ae74d6e0594558a86373f058c74f7
kaiguy920 <smithkai920@gmail.com>
2022-03-09 12:01:57 -0500

    fruits rendering to index route

--- server.js
+++ server.js
@@ -58,6 +58,22 @@ app.get('/fruits/seed', (req, res) => {
     // then we can send if we want to see that data
 })
 
+// INDEX route
+app.get('/fruits', (req, res) => {
+    // find the fruits
+    Fruit.find({})
+        // then render a template AFTER they're found
+        .then(fruits => {
+            console.log(fruits)
+            res.render('fruits/index.liquid', { fruits })
+        })
+        // show an error if there is one
+        .catch(error => {
+            console.log(error)
+            res.json({ error })
+        })
+})
+
 // =============================================================
 //                         SERVER LISTENER
 // =============================================================
