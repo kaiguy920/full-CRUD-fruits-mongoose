cfbc9e29405f8016751bcffd6ce3619a6abdd87a
kaiguy920 <smithkai920@gmail.com>
2022-03-09 10:53:43 -0500

    initial server setup

---
+++ server.js
@@ -0,0 +1,44 @@
+// =============================================================
+//                      IMPORT DEPENDENCIES
+// =============================================================
+// this allows us to load our env variables
+require("dotenv").config()
+const express = require("express")
+const morgan = require("morgan")
+const methodOverride = require("method-override")
+// we'll also import our fruits model when we hav it
+const mongoose = require("mongoose")
+const path = require("path")
+
+// =============================================================
+//           CREATE OUR EXPRESS APPLICATION OBJECT
+// =============================================================
+
+const app = require('liquid-express-views')(express())
+
+// =============================================================
+//                          MIDDLEWARE
+// =============================================================
+// this is for request logging
+app.use(morgan('tiny'))
+app.use(methodOverride('_method'))
+// parses urlencoded request bodies
+app.use(express.urlencoded({ extended: false }))
+// to serve files from public statically
+app.use(express.static('public'))
+
+
+// =============================================================
+//                        ROUTES
+// =============================================================
+app.get('/', (req, res) => {
+    res.send("your server is running")
+})
+// =============================================================
+//                         SERVER LISTENER
+// =============================================================
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`app is listening on port: ${PORT}`);
+})
